a85c55ae24d3e1d3c51381401ea1621d428b7bd3
marcin wrzoszczyk <mwrzos1@students.towson.edu>
2017-10-22 17:06:14 -0400

    Implementation of gittex, title, requiredText

--- src/main/scala/edu/towson/cis/cosc455/mwrzos1/project1/MySyntaxAnalyzer.scala
+++ src/main/scala/edu/towson/cis/cosc455/mwrzos1/project1/MySyntaxAnalyzer.scala
@@ -1,27 +1,75 @@
 package edu.towson.cis.cosc455.mwrzos1.project1
 
+import scala.collection.mutable.ListBuffer
+
 class MySyntaxAnalyzer extends SyntaxAnalyzer {
-  override def gittex(): Unit = ???
 
-  override def title(): Unit = ???
+  //declaration of parse tree
+ var parseTree = ListBuffer[String]()
 
-  override def body(): Unit = ???
+  // Starting production for gittex
+  override def gittex(): Unit = {
 
-  override def paragraph(): Unit = ???
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)) {
 
-  override def heading(): Unit = ???
+      //add to parse tree if it match
+      parseTree.+=(Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
+      variableDefine()
+      title()
+      body()
+      if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCE)) {
+        parseTree.+=(Compiler.currentToken)
+      }
+      else {
+        println("Syntax Error should be: " + CONSTANTS.DOCE + " Received " + Compiler.currentToken)
+        System.exit(1)
+      }
+    }
+    else {
+      println("Syntax Error should be: " + CONSTANTS.DOCB + " Received " + Compiler.currentToken)
+      System.exit(1)
+    }
+  }
+  def title() : Unit = {
 
-  override def variableDefine(): Unit = ???
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.TITLEB)) {
+    parseTree.+=(Compiler.currentToken)
+    Compiler.Scanner.getNextToken()
+    requiredText()
+    if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.BRACKETE)) {
+    parseTree.+=(Compiler.currentToken)
+    }
+    else {
+    println(" Syntax Error should be: " + CONSTANTS.BRACKETE + " Received " + Compiler.currentToken)
+    System.exit(1)
+    }
+   }
+    else {
+    println(" Syntax Error be should be : " + CONSTANTS.TITLEB + " Received " + Compiler.currentToken)
+    System.exit(1)
+    }
+  }
 
-  override def variableUse(): Unit = ???
+  def body() : Unit
+  def paragraph() : Unit
+  def heading() : Unit
+  def variableDefine() : Unit
+  def variableUse() : Unit
+  def bold() : Unit
+  def listItem() : Unit
+  def link() : Unit
+  def image() : Unit
+  def newline() : Unit
 
-  override def bold(): Unit = ???
+  def requiredText(): Unit = {
+    if (Compiler.currentToken.equals(CONSTANTS.specialChars)) {}
+    else  {
+      parseTree.+=(Compiler.currentToken)
+      Compiler.Scanner.getNextToken()
+      requiredText()
+    }
 
-  override def listItem(): Unit = ???
+  }
 
-  override def link(): Unit = ???
-
-  override def image(): Unit = ???
-
-  override def newline(): Unit = ???
-}
+  }
